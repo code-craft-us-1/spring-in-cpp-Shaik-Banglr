95fee0a0aab73a4ca8f26029211e05dfa7c2ada0
Shaik-Banglr <153170456+Shaik-Banglr@users.noreply.github.com>
2024-08-28 12:44:49 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 #include <algorithm>
 #include <numeric> 
-#include <math>
+#include <cmath>
 using namespace Statistics;
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& v ) 
