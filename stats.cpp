912c8e5b79b689932c2b7574c4f9da3ee35c1916
Shaik-Banglr <153170456+Shaik-Banglr@users.noreply.github.com>
2024-08-30 12:28:30 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -6,14 +6,4 @@ using namespace Statistics;
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& v ) 
 {
- Stats obj { NAN, NAN, NAN };
- 
- if (!v.empty())
- {
-	 float sum = static_cast<float>(std::accumulate(v.begin(), v.end(), 0.0));
-	 obj.average = sum / v.size();
-	 obj.max = *std::max_element(v.begin(), v.end());
-	 obj.min = *std::min_element(v.begin(), v.end());
- }
-return obj;
 }
