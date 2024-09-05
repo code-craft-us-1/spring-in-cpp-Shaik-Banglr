7dd60135444142aed049bdbbe8901a8b8aad8372
Shaik-Banglr <153170456+Shaik-Banglr@users.noreply.github.com>
2024-09-05 11:16:02 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,10 +4,23 @@
 
 TEST(Statistics, ReportsAverageMinMax) 
 {
+    auto computedStats = Statistics::ComputeStatistics({ 1.5, 8.9, 3.2, 4.5 });
     double epsilon = 0.001;
+    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
+    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
+    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
 }
 
 TEST(Statistics, AverageNaNForEmpty) 
 {
-    std::vector<float> f = {};
+    std::vector<float> f = {NAN, NAN, NAN, NAN};
+    auto computedStats = Statistics::ComputeStatistics(f);
+    EXPECT_TRUE(std::isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.min));
+    EXPECT_TRUE(std::isnan(computedStats.max));
+
+    //NAN (not-a-number), as defined in math.h
+    
+    //Design the gtest EXPECT statement here.
+    //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
